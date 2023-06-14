6d3e3a9e86ac76e0c878396e3fe4d50ffe6e9076
lawalgodwin <godwinl200@gmail.com>
2023-06-14 13:27:13 +0100

    fix: Fix the Create function bug

--- console.py
+++ console.py
@@ -14,7 +14,7 @@ class HBNBCommand(cmd.Cmd):
     __allObjects = storage.all()
     prompt = '(hbnb) '
 
-    def precmd(self, line):
+    def _precmd(self, line):
         """Preprocess user input before using it for computation"""
         line = line.strip()
         return line
