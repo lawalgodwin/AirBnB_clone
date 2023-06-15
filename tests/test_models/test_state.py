2bc8e5eedabbee39eebd004bd8f1553bddf0d6e3
lawalgodwin <godwinl200@gmail.com>
2023-06-15 11:26:33 +0100

    Add test for the state class

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,76 +1,31 @@
 #!/usr/bin/python3
-"""
-Module of the State unittest
-"""
+"""A test script for the amenity module"""
 
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
-import os
 from models.state import State
 
 
 class TestState(unittest.TestCase):
-    """
-    Test of the State class
-    """
+    """Test case for the state class"""
 
-    # Specific set up of the unittest
     def setUp(self):
-        """Instance of the class"""
-        self.inst = State()
+        # Create an instance of State for testing
+        self.state = State()
 
-    def tearDown(self):
-        """Deleting of the instance with the proper file"""
-        del self.inst
+    def test_attributes(self):
+        # Test the initial attribute values
+        self.assertEqual(self.state.name, "")
 
-        try:
-            os.remove("file.json")
-        except BaseException:
+    def test_inheritance(self):
+        # Test if Amenity inherits from BaseModel
+        self.assertIsInstance(self.state, BaseModel)
 
-            pass
-
-    # Functionality
-    def test_AtributtesClass(self):
-        self.inst.name = "Blessing"
-        self.assertEqual(str, type(self.inst.name))
-        self.assertEqual("Blessing", self.inst.name)
-
-    # Documentation
-    def test_ModuleDocstring(self):
-        """Testing the documentation of the module"""
-        self.assertIsNotNone(State.__doc__)
-
-    def test_MethodsDocstring(self):
-        """Testing the documentation of the different methods"""
-        for doc in dir(State):
-            self.assertIsNotNone(doc.__doc__)
-
-    # Existence and types
-    def test_IsInstance(self):
-        """Testing the existence of the instance"""
-        self.assertIsInstance(self.inst, State)
-
-    def test_TypeId(self):
-        """Test the type of the method id"""
-        self.assertEqual(str, type(self.inst.id))
-
-    def TestFile(self):
-        """The existence of the json file"""
-        self.inst.save()
-        self.assertTrue(os.path.isfile("file.json"))
-
-    def test_Methods(self):
-        """Testing the existence of the different methods"""
-        self.assertTrue(hasattr(State, "name"))
-
-    def test_ClassDict(self):
-        """Testing the dictionary of the class"""
-        ClassDict = self.inst.to_dict()
-        self.assertEqual(dict, type(ClassDict))
-        self.assertIsInstance(ClassDict["created_at"], str)
-        self.assertIsInstance(ClassDict["updated_at"], str)
+    def test_setting_attributes(self):
+        # Test setting attribute values
+        self.state.name = "Swimming Pool"
+        self.assertEqual(self.state.name, "Swimming Pool")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
